a74b165111082ad376dbf4dd22ed4c19553dd3b3
halloween-jack <halloween.jack.dev@gmail.com>
2017-11-17 20:32:34 +0900

    Initial Commit

---
+++ Project37/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project37
+//
+//  Created by slim on 2017/11/17.
+//  Copyright © 2017 halloween-jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
